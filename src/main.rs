5480f807c3f68e8981bfd98a4e908eca089652b0
Brian Dawn <brian.t.dawn@gmail.com>
2023-10-21 08:09:07 -0500

    Initialize the repo.

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::{error::Error, net::SocketAddr};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> Result<Html<String>, StatusCode> {
+    #[derive(Template)] // this will generate the code...
+    #[template(path = "index.html")] // using the template in this path, relative
+    struct IndexTemplate<'a> {
+        // the name of the struct can be anything
+        name: &'a str, // the field name should match the variable name
+                       // in your template
+    }
+
+    // Generate askama template
+    let index = IndexTemplate { name: "world" };
+    let rendered = index.render().map_err(|e| {
+        tracing::error!("Failed to render template: {}", e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+
+    Ok(Html(rendered))
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
